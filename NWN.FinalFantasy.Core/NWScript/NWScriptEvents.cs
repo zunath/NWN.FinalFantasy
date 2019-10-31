817df2fecfc50cea2aaa8d3030e9b35319df629e
Zunath <coolty3001@yahoo.com>
2019-10-30 20:54:39 -0400

    Initial commit

---
+++ NWN.FinalFantasy.Core/NWScript/NWScriptEvents.cs
@@ -0,0 +1,13 @@
+﻿// ReSharper disable once CheckNamespace
+namespace NWN
+{
+    public class Events
+    {
+        public delegate void MainLoopDelegate(ulong frame);
+        public static event MainLoopDelegate MainLoopTick;
+        public static void OnMainLoopTick(ulong frame)
+        {
+            MainLoopTick?.Invoke(frame);
+        }
+    }
+}

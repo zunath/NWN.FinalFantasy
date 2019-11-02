be669a2e9b6fa0ea3f5816368195ec662206528c
Zunath <coolty3001@yahoo.com>
2019-11-02 13:27:07 -0400

    Add NWNX Dialog

---
+++ NWN.FinalFantasy.Core/NWNX/DialogNodeType.cs
@@ -0,0 +1,10 @@
+﻿namespace NWN.FinalFantasy.Core.NWNX
+{
+    public enum DialogNodeType
+    {
+        Invalid = -1,
+        Starting = 0,
+        Entry = 1,
+        Reply = 2
+    }
+}
---
+++ NWN.FinalFantasy.Core/NWNX/DialogScriptType.cs
@@ -0,0 +1,9 @@
+﻿namespace NWN.FinalFantasy.Core.NWNX
+{
+    public enum DialogScriptType
+    {
+        Other = 0,
+        Conditional = 1,
+        ActionTaken = 2
+    }
+}

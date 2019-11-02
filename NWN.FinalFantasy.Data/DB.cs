e184cb4d3c069ee5fa70da58ca1c79d55d809a9e
Zunath <coolty3001@yahoo.com>
2019-11-02 12:11:05 -0400

    Use NWNX's Redis implementation because it's quicker than StackExchange.Redis for some reason. Add a couple docker files for easier development.

--- NWN.FinalFantasy.Data/DB.cs
+++ NWN.FinalFantasy.Data/DB.cs
@@ -1,53 +1,11 @@
 ﻿using System;
 using Newtonsoft.Json;
-using StackExchange.Redis;
+using NWN.FinalFantasy.Core.NWNX;
 
 namespace NWN.FinalFantasy.Data
 {
     public static class DB
     {
-        private static ConnectionMultiplexer Connection { get; }
-
-        static DB()
-        {
-            var settings = new ApplicationSettings();
-            var uri = settings.Host + ":" + settings.Port;
-            Connection = ConnectionMultiplexer.Connect(uri);
-        }
-
-        /// <summary>
-        /// Subscribes to a Redis event. Whenever an event matching this object type is raised, the specified action will be executed.
-        /// These events may come internally from the server OR external sources like the website or Discord.
-        /// </summary>
-        /// <typeparam name="T">The event type</typeparam>
-        /// <param name="action">The action to run when the event is raised.</param>
-        public static void Subscribe<T>(Action<T> action)
-            where T: class
-        {
-            var sub = Connection.GetSubscriber();
-            var channel = typeof(T).FullName;
-
-            sub.Subscribe(channel, (c, message) =>
-            {
-                T payload = JsonConvert.DeserializeObject<T>(message);
-                action(payload);
-            });
-        }
-
-        /// <summary>
-        /// Publishes a new Redis event. All subscribers currently listening to this event will execute their actions.
-        /// </summary>
-        /// <typeparam name="T">The event type</typeparam>
-        /// <param name="payload">The data to transfer to subscribers listening for this event.</param>
-        public static void Publish<T>(T payload)
-        {
-            var sub = Connection.GetSubscriber();
-            var channel = typeof(T).FullName;
-            var data = JsonConvert.SerializeObject(payload);
-
-            sub.Publish(channel, data);
-        }
-
         /// <summary>
         /// Stores a specific object in the database by its ID.
         /// </summary>
@@ -58,14 +16,10 @@ namespace NWN.FinalFantasy.Data
         {
             var @namespace = typeof(T).FullName;
             var id = entity.ID.ToString();
-            var key = @namespace + "::" + id;
+            var key = @namespace + ":" + id;
             var data = JsonConvert.SerializeObject(entity);
 
-            var db = Connection.GetDatabase();
-            var success = db.StringSet(key, data);
-
-            if(!success)
-                throw new Exception($"Failed to set value for key '{key}");
+            NWNXRedis.Set(key, data);
         }
 
         /// <summary>
@@ -78,10 +32,9 @@ namespace NWN.FinalFantasy.Data
             where T: Entity
         {
             var @namespace = typeof(T).FullName;
-            var key = @namespace + "::" + id;
-            var db = Connection.GetDatabase();
+            var key = @namespace + ":" + id;
 
-            var json = db.StringGet(key);
+            var json = NWNXRedis.Get(key);
             return JsonConvert.DeserializeObject<T>(json);
         }
     }

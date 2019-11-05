56cdb7cdeb0814ea4ae933429523935437c70895
Zunath <coolty3001@yahoo.com>
2019-11-05 06:09:10 -0500

    Remove unnecessary json converter

--- NWN.FinalFantasy.Data/Entity/InventoryItem.cs
+++ NWN.FinalFantasy.Data/Entity/InventoryItem.cs
@@ -1,8 +1,4 @@
-﻿using Newtonsoft.Json;
-using NWN.FinalFantasy.Core.NWNX;
-using NWN.FinalFantasy.Data.JsonConverters;
-
-namespace NWN.FinalFantasy.Data.Entity
+﻿namespace NWN.FinalFantasy.Data.Entity
 {
     public class InventoryItem: EntityBase
     {

c5b417691cec7efbea445946c573532e64fdaacd
Zunath <coolty3001@yahoo.com>
2019-11-12 19:52:23 -0500

    Apply Defense Bonus Trait logic

---
+++ NWN.FinalFantasy.Job/Scripts/Traits/ApplyDefenseBonusTrait.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NWN.FinalFantasy.Core;
+using NWN.FinalFantasy.Core.NWNX;
+using NWN.FinalFantasy.Core.NWScript.Enumerations;
+using NWN.FinalFantasy.Job.Event;
+
+namespace NWN.FinalFantasy.Job.Scripts.Traits
+{
+    /// <summary>
+    /// Handles adding bonus AC to players with the Defense Bonus feats.
+    /// </summary>
+    internal class ApplyDefenseBonusTrait: StatProcessingShared
+    {
+        public static void Main()
+        {
+            var data = Script.GetScriptData<StatsRecalculated>();
+            var player = data.Player;
+            int baseAC = NWNXCreature.GetBaseAC(player);
+            int increaseBy;
+
+            if (_.GetHasFeat(Feat.DefenseBonus1, player))
+            {
+                increaseBy = 3;
+            }
+            else
+            {
+                return;
+            }
+
+            baseAC += increaseBy;
+            NWNXCreature.SetBaseAC(player, baseAC);
+        }
+    }
+}

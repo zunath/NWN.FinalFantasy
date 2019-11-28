9e8679ae898af8473c1c9ef1e2a00ea5d6bd47f6
Zunath <coolty3001@yahoo.com>
2019-11-28 15:49:38 -0500

    Spring Attack + Whirlwind Attack

---
+++ NWN.FinalFantasy.Job/AbilityDefinition/Chi/WhirlwindAttack.cs
@@ -0,0 +1,48 @@
+﻿using NWN.FinalFantasy.Core.NWScript.Enumerations;
+using NWN.FinalFantasy.Job.Enumeration;
+using static NWN._;
+
+namespace NWN.FinalFantasy.Job.AbilityDefinition.StatBoost
+{
+    internal class WhirlwindAttack : IAbilityDefinition
+    {
+        public string Name => "Whirlwind Attack";
+        public Feat Feat => Feat.WhirlwindAttack;
+        public AbilityCategory Category => AbilityCategory.Combat;
+        public AbilityGroup Group => AbilityGroup.Individual;
+        public bool IsEquippable => false;
+        public int APRequired => 90;
+
+        public JobLevel[] JobRequirements => new[]
+        {
+            new JobLevel(ClassType.Monk, 13),
+        };
+        public int MP(NWGameObject user)
+        {
+            return 0;
+        }
+
+        public string CanUse(NWGameObject user, NWGameObject target)
+        {
+            return null;
+        }
+
+        public float CastingTime(NWGameObject user)
+        {
+            return 0f;
+        }
+
+        public float CooldownTime(NWGameObject user)
+        {
+            return 0f;
+        }
+
+        public void Apply(NWGameObject user)
+        {
+        }
+
+        public void Impact(NWGameObject user, NWGameObject target)
+        {
+        }
+    }
+}

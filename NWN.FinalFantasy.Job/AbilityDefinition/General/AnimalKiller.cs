febe1e08c84e99b34a6e7582efc2715434e59c3c
Zunath <coolty3001@yahoo.com>
2019-12-01 15:31:41 -0500

    Add Killer abilities and Weapon Finesse ability.

---
+++ NWN.FinalFantasy.Job/AbilityDefinition/General/AnimalKiller.cs
@@ -0,0 +1,52 @@
+﻿using NWN.FinalFantasy.Core.NWScript.Enumerations;
+using NWN.FinalFantasy.Job.Enumeration;
+
+namespace NWN.FinalFantasy.Job.AbilityDefinition.General
+{
+    internal class AnimalKiller : IAbilityDefinition
+    {
+        public string Name => "Animal Killer";
+        public Feat Feat => Feat.FavoredEnemyAnimal;
+        public AbilityCategory Category => AbilityCategory.Trait;
+        public AbilityGroup Group => AbilityGroup.Individual;
+        public bool IsEquippable => true;
+        public int APRequired => 40;
+
+        public JobLevel[] JobRequirements => new[]
+        {
+            new JobLevel(ClassType.Warrior, 5),
+            new JobLevel(ClassType.Monk, 5),
+            new JobLevel(ClassType.Thief, 5),
+            new JobLevel(ClassType.Ranger, 5),
+            new JobLevel(ClassType.WhiteMage, 5),
+            new JobLevel(ClassType.BlackMage, 5),
+        };
+        public int MP(NWGameObject user)
+        {
+            return 0;
+        }
+
+        public string CanUse(NWGameObject user, NWGameObject target)
+        {
+            return null;
+        }
+
+        public float CastingTime(NWGameObject user)
+        {
+            return 0;
+        }
+
+        public float CooldownTime(NWGameObject user)
+        {
+            return 0;
+        }
+
+        public void Apply(NWGameObject user)
+        {
+        }
+
+        public void Impact(NWGameObject user, NWGameObject target)
+        {
+        }
+    }
+}
---
+++ NWN.FinalFantasy.Job/AbilityDefinition/General/ConstructKiller.cs
@@ -0,0 +1,52 @@
+﻿using NWN.FinalFantasy.Core.NWScript.Enumerations;
+using NWN.FinalFantasy.Job.Enumeration;
+
+namespace NWN.FinalFantasy.Job.AbilityDefinition.General
+{
+    internal class ConstructKiller : IAbilityDefinition
+    {
+        public string Name => "Construct Killer";
+        public Feat Feat => Feat.FavoredEnemyConstruct;
+        public AbilityCategory Category => AbilityCategory.Trait;
+        public AbilityGroup Group => AbilityGroup.Individual;
+        public bool IsEquippable => true;
+        public int APRequired => 50;
+
+        public JobLevel[] JobRequirements => new[]
+        {
+            new JobLevel(ClassType.Warrior, 10),
+            new JobLevel(ClassType.Monk, 10),
+            new JobLevel(ClassType.Thief, 10),
+            new JobLevel(ClassType.Ranger, 10),
+            new JobLevel(ClassType.WhiteMage, 10),
+            new JobLevel(ClassType.BlackMage, 10),
+        };
+        public int MP(NWGameObject user)
+        {
+            return 0;
+        }
+
+        public string CanUse(NWGameObject user, NWGameObject target)
+        {
+            return null;
+        }
+
+        public float CastingTime(NWGameObject user)
+        {
+            return 0;
+        }
+
+        public float CooldownTime(NWGameObject user)
+        {
+            return 0;
+        }
+
+        public void Apply(NWGameObject user)
+        {
+        }
+
+        public void Impact(NWGameObject user, NWGameObject target)
+        {
+        }
+    }
+}

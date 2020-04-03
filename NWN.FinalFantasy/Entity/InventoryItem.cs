5849443d194c02b8af3d5aa79969eeacf5207ca8
Zunath <coolty3001@yahoo.com>
2020-04-02 21:34:06 -0400

    Persistent bank storage

---
+++ NWN.FinalFantasy/Entity/InventoryItem.cs
@@ -0,0 +1,11 @@
+﻿namespace NWN.FinalFantasy.Entity
+{
+    public class InventoryItem : EntityBase
+    {
+        public string Name { get; set; }
+        public string Tag { get; set; }
+        public string Resref { get; set; }
+        public int Quantity { get; set; }
+        public string Data { get; set; }
+    }
+}
--- NWN.FinalFantasy/Service/Item.cs
+++ NWN.FinalFantasy/Service/Item.cs
@@ -21,5 +21,25 @@ namespace NWN.FinalFantasy.Service
             }
         }
 
+        /// <summary>
+        /// Returns the number of items in an object's inventory.
+        /// Returns -1 if target does not have an inventory
+        /// </summary>
+        /// <param name="obj">The object to check</param>
+        /// <returns>-1 if obj doesn't have an inventory, otherwise returns the number of items in the inventory</returns>
+        public static int GetInventoryItemCount(uint obj)
+        {
+            if (!GetHasInventory(obj)) return -1;
+
+            int count = 0;
+            var item = GetFirstItemInInventory(obj);
+            while (GetIsObjectValid(item))
+            {
+                count++;
+                item = GetNextItemInInventory(obj);
+            }
+
+            return count;
+        }
     }
 }

5e07ed72c0608c24d022cb9e0fa116a5afdf05f6
Zunath <coolty3001@yahoo.com>
2020-05-02 15:47:51 -0400

    Persistent HP

---
+++ NWN.FinalFantasy/Feature/PersistentHitPoints.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using NWN.FinalFantasy.Core;
+using NWN.FinalFantasy.Core.NWScript.Enum;
+using NWN.FinalFantasy.Entity;
+using NWN.FinalFantasy.Service;
+using static NWN.FinalFantasy.Core.NWScript.NWScript;
+
+namespace NWN.FinalFantasy.Feature
+{
+    public static class PersistentHitPoints
+    {
+        /// <summary>
+        /// When a player leaves the server, save their persistent HP.
+        /// </summary>
+        [NWNEventHandler("mod_exit")]
+        public static void SaveHP()
+        {
+            var player = GetExitingObject();
+            if (!GetIsPC(player) || GetIsDM(player)) return;
+
+            var playerId = GetObjectUUID(player);
+            var dbPlayer = DB.Get<Player>(playerId);
+            dbPlayer.HP = GetCurrentHitPoints(player);
+
+            DB.Set(playerId, dbPlayer);
+        }
+
+        /// <summary>
+        /// When a player enters the server, load their persistent HP.
+        /// </summary>
+        [NWNEventHandler("mod_enter")]
+        public static void LoadHP()
+        {
+            var player = GetEnteringObject();
+            if (!GetIsPC(player) || GetIsDM(player)) return;
+
+            var playerId = GetObjectUUID(player);
+            var dbPlayer = DB.Get<Player>(playerId);
+            if (dbPlayer == null) return;
+
+            int hp = GetCurrentHitPoints(player);
+            int damage;
+            if (dbPlayer.HP < 0)
+            {
+                damage = hp + Math.Abs(dbPlayer.HP);
+            }
+            else
+            {
+                damage = hp - dbPlayer.HP;
+            }
+
+            if (damage != 0)
+            {
+                ApplyEffectToObject(DurationType.Instant, EffectDamage(damage), player);
+            }
+        }
+    }
+}

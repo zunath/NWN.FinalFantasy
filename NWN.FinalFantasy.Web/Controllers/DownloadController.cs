6df2b3ee9dcf7df63903c527f84f8f85bf733e71
Z <coolty3001@yahoo.com>
2020-06-04 22:30:31 -0400

    Add website. Downloads, gallery, home, links to external sites.

---
+++ NWN.FinalFantasy.Web/Controllers/DownloadController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NWN.FinalFantasy.Web.Controllers
+{
+    public class DownloadController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult DownloadHaks()
+        {
+            // Could live in a DB or something, but don't want to complicate it.
+            const string LocalPath = "/var/www/ffo_public_files/FFOHaks.rar";
+            const string ContentType = "application/octet-stream";
+            const string FileName = "FFODevelopmentHaks.rar";
+
+            if (!System.IO.File.Exists(LocalPath))
+            {
+                return Content("Could not find file.");
+            }
+
+            return File(System.IO.File.OpenRead(LocalPath), ContentType, FileName);
+        }
+    }
+}

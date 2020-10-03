f696938dac631c6b795e0945d14a0a257ee3abfe
Z <coolty3001@yahoo.com>
2020-10-03 18:26:04 -0400

    Add profiler around AIPlayerData.CaptureData() call.

--- NWN.FinalFantasy/Feature/AIDefinition/AIData/AIPlayerData.cs
+++ NWN.FinalFantasy/Feature/AIDefinition/AIData/AIPlayerData.cs
@@ -3,6 +3,7 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
+using NWN.FinalFantasy.Core;
 using NWN.FinalFantasy.Entity;
 using NWN.FinalFantasy.Service;
 using NWN.FinalFantasy.Service.AIService;
@@ -35,27 +36,31 @@ namespace NWN.FinalFantasy.Feature.AIDefinition.AIData
         /// </summary>
         public void CaptureDataMainThread()
         {
-            PlayerData.Clear();
-
-            for (var player = GetFirstPC(); GetIsObjectValid(player); player = GetNextPC())
+            using (new Profiler($"{nameof(AIPlayerData)}:{nameof(CaptureDataMainThread)}"))
             {
-                var playerId = GetObjectUUID(player);
-                var dbPlayer = DB.Get<Player>(playerId);
+                PlayerData.Clear();
 
-                PlayerData[player] = new AIPlayer
+                for (var player = GetFirstPC(); GetIsObjectValid(player); player = GetNextPC())
                 {
-                    Player = player,
-                    CurrentHP = GetCurrentHitPoints(player),
-                    MaxHP = GetMaxHitPoints(player),
-                    MaxMP = Stat.GetMaxMP(player, dbPlayer),
-                    CurrentMP = dbPlayer.MP,
-                    MaxSTM = Stat.GetMaxStamina(player, dbPlayer),
-                    CurrentSTM = dbPlayer.Stamina,
-                    Position = GetPosition(player),
-                    Area = GetArea(player),
-                    Facing = GetFacing(player)
-                };
+                    var playerId = GetObjectUUID(player);
+                    var dbPlayer = DB.Get<Player>(playerId);
+
+                    PlayerData[player] = new AIPlayer
+                    {
+                        Player = player,
+                        CurrentHP = GetCurrentHitPoints(player),
+                        MaxHP = GetMaxHitPoints(player),
+                        MaxMP = Stat.GetMaxMP(player, dbPlayer),
+                        CurrentMP = dbPlayer.MP,
+                        MaxSTM = Stat.GetMaxStamina(player, dbPlayer),
+                        CurrentSTM = dbPlayer.Stamina,
+                        Position = GetPosition(player),
+                        Area = GetArea(player),
+                        Facing = GetFacing(player)
+                    };
+                }
             }
+            
         }
 
         /// <summary>

42ef46cd5fd7b7c518143cc86e26f4a4a2fdadd4
Z <coolty3001@yahoo.com>
2020-10-18 17:20:31 -0400

    Add action-open-store snippet.

---
+++ NWN.FinalFantasy/Feature/SnippetDefinition/MerchantSnippetDefinition.cs
@@ -0,0 +1,35 @@
+﻿using NWN.FinalFantasy.Core.NWScript;
+using NWN.FinalFantasy.Core.NWScript.Enum;
+using NWN.FinalFantasy.Service;
+using static NWN.FinalFantasy.Core.NWScript.NWScript;
+
+namespace NWN.FinalFantasy.Feature.SnippetDefinition
+{
+    public static class MerchantSnippetDefinition
+    {
+        /// <summary>
+        /// Snippet which opens a store. If store tag isn't specified,
+        /// the nearest store to the NPC will be opened.
+        /// </summary>
+        /// <param name="player">The player opening the store.</param>
+        /// <param name="args">Arguments provided by conversation builder.</param>
+        [Snippet("action-open-store")]
+        public static void OpenStore(uint player, string[] args)
+        {
+            var npc = OBJECT_SELF;
+            var store = GetNearestObject(ObjectType.Store, npc);
+            if (args.Length > 0)
+            {
+                var storeTag = args[0];
+                store = GetNearestObjectByTag(storeTag, npc);
+            }
+
+            if (!GetIsObjectValid(store))
+            {
+                Log.Write(LogGroup.Error, $"{GetName(npc)} could not locate a valid store. Check conversation for incorrect snippet parameters.", true);
+            }
+
+            NWScript.OpenStore(store, player);
+        }
+    }
+}
